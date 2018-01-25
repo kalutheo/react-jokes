9e12e704435c1dff16ecd264fe50075002046ff5
Théophile KALUMBU <kalutheo@hotmail.com>
2018-01-25 13:59:44 +0100

    feat: add react-loader for smooth loadiong

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import RemoteData from "remote-data-js";
+import ContentLoader from "react-content-loader";
+
+const MyLoader = () => (
+  <ContentLoader
+    height={60}
+    speed={2}
+    primaryColor={"#f3f3f3"}
+    secondaryColor={"#ecebeb"}
+  >
+    <rect x="-3" y="8" rx="3" ry="3" width="350" height="10" />
+    <rect x="-3" y="28" rx="3" ry="3" width="400" height="10" />
+    <rect x="-3" y="48" rx="3" ry="3" width="360" height="10" />
+  </ContentLoader>
+);
 
 const Layout = ({ children, isPending, fetch }) => (
   <div className="p-8 bg-white shadow rounded">
     <h1 className="text-green mb-2">Chuck Norris Jokes</h1>
     <div className="display-block">{children}</div>
     <button
-      className="rounded bg-green text-white p-2 mt-2 hover:bg-green-dark disabled:bg-grey"
+      className="rounded bg-green text-white p-1 mt-2 hover:bg-green-dark disabled:bg-grey"
       disabled={isPending}
       onClick={fetch}
     >
@@ -15,6 +29,10 @@ const Layout = ({ children, isPending, fetch }) => (
   </div>
 );
 const Joke = ({ joke }) => <div>{joke.value}</div>;
+
+const ErrorMessage = ({ label }) => (
+  <div className="bg-red-lighter p-2 text-red-darker rounded">{label}</div>
+);
 
 class Home extends Component {
   state = { data: null };
@@ -35,9 +53,9 @@ class Home extends Component {
     const requestContent = data
       ? data.case({
           NotAsked: () => null,
-          Pending: () => "Loading...",
+          Pending: () => <MyLoader />,
           Success: data => <Joke joke={data} />,
-          Failure: error => "An error occured :-("
+          Failure: error => <ErrorMessage label="An error occured :-(" />
         })
       : null;
     return (
